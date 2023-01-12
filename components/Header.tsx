65a0a3a26cdb4768ec3d38adad42a48078b54f31
Zhayu517 <zhangzy517@gmail.com>
2023-01-12 23:20:07 +0000

    dark mode button now changes color for Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,43 +1,46 @@
+import React, { useState } from 'react'
 import styles from '../styles/Header.module.css'
+import '../styles/Header.module.css'
 import Link from 'next/link'
 
 type Props = {}
 
 function Header({}: Props) {
-  return (
-    <header className={styles.header}>
-        <div className={styles.section_one}>
-          <div className={styles.logoContainer}>
-            <Link href="/">
-                Zhaoyu Zhang
-                {/* ##### #   #   #   #    #   ###  
-                   #  #   #  # #  # #  #  # 
-                  #   ##### ##### #  # # #  ## 
-                 #    #   # #   # #   ## #   # 
-                ##### #   # #   # #    #   ###
-                ###### #    #   ##   #    # #####  #      ######
-                #       #  #   #  #  ##  ## #    # #      #      
-                #####    ##   #    # # ## # #    # #      ##### 
-                #        ##   ###### #    # #####  #      #      
-                #       #  #  #    # #    # #      #      #      
-                ###### #    # #    # #    # #      ###### ###### */}
-            </Link>
-          </div>
-        </div>
 
-        <div className={styles.section_two}>
-            <div className={styles.linkContainer}>
-              <Link href="/">Projects</Link>
-              <Link href="/cooking/">Cooking</Link>
-              <Link href="/life/">Life</Link> 
-              <Link href="/aboutme/">About Me</Link>
+  const [mode, setMode] = useState("light");
+
+  const toggleMode = () => {
+      setMode(mode === "light" ? "dark" : "light");
+  };
+
+  return (
+      <header style={{ backgroundColor: mode === "light" ? "white" : "black" }} className={styles.header}>
+          <div className={styles.section_one}>
+            <div className={styles.logoContainer}>
+              <a href="/" style={{ color: mode === "light" ? "black" : "white" }} >
+                  Zhaoyu Zhang
+                  {/* ##### #   #   #   #    #   ###  
+                    #  #   #  # #  # #  #  # 
+                    #   ##### ##### #  # # #  ## 
+                  #    #   # #   # #   ## #   # 
+                  ##### #   # #   # #    #   ### */}
+              </a>
             </div>
-            <button id="dark-mode-button" className={styles.darkModeToggler}>
-              <div className={styles.darkModeTogglerIcon}>
+          </div>
+
+          <div className={styles.section_two}>
+              <div className={styles.linkContainer}>
+                <a href="/" style={{ color: mode === "light" ? "black" : "white" }}>Projects</a>
+                <a href="/cooking/" style={{ color: mode === "light" ? "black" : "white" }}>Cooking</a>
+                <a href="/life/" style={{ color: mode === "light" ? "black" : "white" }}>Life</a> 
+                <a href="/aboutme/" style={{ color: mode === "light" ? "black" : "white" }}>About Me</a>
               </div>
-            </button>
-        </div>
-    </header> 
+              <button className={styles.modeToggler} onClick={toggleMode}>
+                <div className={styles.modeTogglerIcon}>
+                </div>
+              </button>
+          </div>
+      </header>
   )
 }
 
