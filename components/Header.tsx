5fe5345385854e8403ade7b297741e675ace5bb9
Zhayu517 <zhangzy517@gmail.com>
2023-01-13 02:23:37 +0000

    Prepare for add dark-mode

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,23 +5,22 @@ import Link from 'next/link'
 type Props = {}
 
 function Header({}: Props) {
-
-  const iconRef = useRef<HTMLDivElement>(null);
-  const [move, setMove] = useState(false);
-
-  const [mode, setMode] = useState("light");
+  // const [darkMode, setDarkMode] = useState(true);
 
   const toggleMode = () => {
-      setMode(mode === "light" ? "dark" : "light");
-      setMove(!move);
+      // setDarkMode(!darkMode);
+      var element = document.body;
+      element.classList.toggle("dark-mode");
   };
 
   return (
-      <header style={{ backgroundColor: mode === "light" ? "white" : "black" }} className={styles.header}>
+      // <header style={{ backgroundColor: darkMode ? "white" : "black" }} className={styles.header}>
+      <header className={styles.header}>
           <div className={styles.section_one}>
             <div className={styles.logoContainer}>
               <Link href="/" >
-                <a style={{ color: mode === "light" ? "black" : "white" }} >
+                {/* <a style={{ color: darkMode ? "black" : "white" }} > */}
+                <a>
                   Zhaoyu Zhang
                 </a>
                </Link>
@@ -31,29 +30,33 @@ function Header({}: Props) {
           <div className={styles.section_two}>
               <div className={styles.linkContainer}>
                 <Link href="/" >
-                  <a style={{ color: mode === "light" ? "black" : "white"}}>
+                  {/* <a style={{ color: darkMode ? "black" : "white" }}> */}
+                  <a>
                     Projects
                   </a>
                 </Link>
                 <Link href="/cooking/" >
-                  <a style={{ color: mode === "light" ? "black" : "white" }}>
+                  {/* <a style={{ color: darkMode ? "black" : "white" }}> */}
+                  <a>
                     Cooking
                   </a>
                 </Link>
                 <Link href="/life/" >
-                  <a style={{ color: mode === "light" ? "black" : "white" }}>
+                  {/* <a style={{ color: darkMode ? "black" : "white" }}> */}
+                  <a>
                     Life
                   </a>
                 </Link> 
                 <Link href="/aboutme/" >
-                  <a style={{ color: mode === "light" ? "black" : "white" }}>
+                  {/* <a style={{ color: darkMode ? "black" : "white" }}> */}
+                  <a>
                     About Me
                   </a>
                 </Link>
               </div>
               <button className={styles.modeToggler} onClick={toggleMode}>
-                {/* <div ref={iconRef} className={`${styles.modeTogglerIcon} ${move ? "move" : ""}`} /> */}
-                <div ref={iconRef} className={`${styles.modeTogglerIcon} `} />
+                {/* <div className={`${styles.modeTogglerIcon} ${darkMode ? "" : "move"}`} /> */}
+                <div className={`${styles.modeTogglerIcon}`} />
               </button>
           </div>
       </header>
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -7,18 +7,6 @@ function Projects({}: Props) {
   return (
     <main className={styles.main}>
         <div className={styles.contentContainer}>
-            {/* <div className={styles.imageContainer}>
-              <img className={styles.image} src={'/images/me.jpg'} alt="My Image" />
-            </div>
-
-            <h1 className={styles.title}>
-                <Link href="/">Zhaoyu Zhang</Link>
-            </h1>
-
-            <p className={styles.description}>
-                Get Connected with Me!
-            </p> */}
-
             <p className={styles.description}>
               What Obsessed Me <br/>
               Shapes My Future.
