301b1bef5ebf8f8a1a4014936c55526572fd2eae
Zhayu517 <zhangzy517@gmail.com>
2023-01-13 21:21:46 +0000

    DarkMode now enables animation performs correctly

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,22 +5,28 @@ import Link from 'next/link'
 type Props = {}
 
 function Header({}: Props) {
-  // const [darkMode, setDarkMode] = useState(true);
-
+  const [darkMode, setDarkMode] = useState(true);
+  const backGroundwhiteBlack = {backgroundColor: darkMode ? "white" : "black"};
+  const colorblackWhite = {
+    color: darkMode ? "black" : "white",
+    backgroundColor: darkMode ? "white" : "black",
+  };
+  const linkContainer = darkMode ? styles.linkContainer : styles.linkContainerDark;
   const toggleMode = () => {
-      // setDarkMode(!darkMode);
-      var element = document.body;
-      element.classList.toggle("dark-mode");
+      setDarkMode(!darkMode);
+      // var element = document.body;
+      // element.classList.toggle("dark-mode");
   };
 
   return (
-      // <header style={{ backgroundColor: darkMode ? "white" : "black" }} className={styles.header}>
-      <header className={styles.header}>
+    /* <body> */
+      <header style={backGroundwhiteBlack} className={styles.header}>
+      {/* <header className={styles.header}> */}
           <div className={styles.section_one}>
             <div className={styles.logoContainer}>
               <Link href="/" >
-                {/* <a style={{ color: darkMode ? "black" : "white" }} > */}
-                <a>
+                <a style={colorblackWhite} >
+                {/* <a> */}
                   Zhaoyu Zhang
                 </a>
                </Link>
@@ -28,38 +34,39 @@ function Header({}: Props) {
           </div>
 
           <div className={styles.section_two}>
-              <div className={styles.linkContainer}>
+              <div className={linkContainer}>
                 <Link href="/" >
-                  {/* <a style={{ color: darkMode ? "black" : "white" }}> */}
-                  <a>
+                  <a style={colorblackWhite}>
+                  {/* <a> */}
                     Projects
                   </a>
                 </Link>
                 <Link href="/cooking/" >
-                  {/* <a style={{ color: darkMode ? "black" : "white" }}> */}
-                  <a>
+                  <a style={colorblackWhite}>
+                  {/* <a> */}
                     Cooking
                   </a>
                 </Link>
                 <Link href="/life/" >
-                  {/* <a style={{ color: darkMode ? "black" : "white" }}> */}
-                  <a>
+                  <a style={colorblackWhite}>
+                  {/* <a> */}
                     Life
                   </a>
                 </Link> 
                 <Link href="/aboutme/" >
-                  {/* <a style={{ color: darkMode ? "black" : "white" }}> */}
-                  <a>
+                  <a style={colorblackWhite}>
+                  {/* <a> */}
                     About Me
                   </a>
                 </Link>
               </div>
               <button className={styles.modeToggler} onClick={toggleMode}>
-                {/* <div className={`${styles.modeTogglerIcon} ${darkMode ? "" : "move"}`} /> */}
-                <div className={`${styles.modeTogglerIcon}`} />
+                <div className={`${styles.modeTogglerIcon} ${darkMode ? "" : "move"}`} />
+                {/* <div className={`${styles.modeTogglerIcon}`} /> */}
               </button>
           </div>
       </header>
+      // {/* </body> */}
   )
 }
 
