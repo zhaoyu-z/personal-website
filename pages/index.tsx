3cf874d6331239cbcc611d126df3cdbadabc91f3
Zhayu517 <zhangzy517@gmail.com>
2023-05-03 23:50:13 +0100

    create theme default to system mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,28 @@
+import * as React from 'react'
 import type { NextPage } from 'next'
 import Temp from '../components/temp'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import useMediaQuery from '@mui/material/useMediaQuery'
 
 const Home: NextPage = () => {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+  );
+
   return (
-    <Temp />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Temp />
+    </ThemeProvider>
   )
 }
 
