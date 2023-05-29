037f21c9df71e74faf45a203d2671baf63f41db4
Zhayu517 <zhangzy517@gmail.com>
2023-05-29 14:02:56 +0100

    added animation to intro

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -5,6 +5,7 @@ import {
     CssBaseline,
     Paper
 } from '@mui/material'
+import { TypeAnimation } from 'react-type-animation'
 import styles from '../styles/Intro.module.css'
 
 type IntroProps = {
@@ -19,9 +20,27 @@ function Intro(props?: IntroProps) {
             sx={{ ...props?.additionalStyles }}
         >
             <CssBaseline />
-            <Typography sx={{ color: "white", fontSize: "2vw" }}>
+            {/* <Typography sx={{ color: "white", fontSize: "2vw" }}>
                 {"Hi, I'm Zhaoyu Zhang"}
-            </Typography>
+            </Typography> */}
+            <TypeAnimation
+                sequence={[
+                    // Same String at the start will only be typed once, initially
+                    'Hi, I\'m Zhaoyu Zhang',
+                    1000,
+                    'Welcome to My Personal Website!',
+                    1000,
+                    'I\'m a Software Engineer',
+                    1000,
+                    'Contact Me if You Need Anything!',
+                    1000,
+                ]}
+                speed={50}
+                style={{ fontSize: '5em' }}
+                repeat={Infinity}
+                cursor={false}
+                className={styles.type}
+            />
         </Box>
     )
 }
