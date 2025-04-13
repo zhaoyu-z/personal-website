a188d40bfad173c656a448640721876652858faa
zhaoyu-z <zhangzy517@gmail.com>
2025-04-13 23:37:18 +0100

    update timeline

--- components/config/TimeLine.config.tsx
+++ components/config/TimeLine.config.tsx
@@ -69,13 +69,13 @@ export const events = [
 		image: "",
 		icon: <WorkIcon className={styles.icon} />
     },
-    {
-		primary: "Met My Love",
-		secondary: "I met my lovely girlfriend, Lynn.",
-		time: new Date("April 29, 2022"),
-		image: "",
-		icon: <FavoriteIcon className={styles.icon} />
-    },
+    // {
+	// 	primary: "Met My Love",
+	// 	secondary: "I met my lovely girlfriend, Lynn.",
+	// 	time: new Date("April 29, 2022"),
+	// 	image: "",
+	// 	icon: <FavoriteIcon className={styles.icon} />
+    // },
     {
 		primary: "Graduation",
 		secondary: "I graduated from the <b>University of Manchester</b>. Big Step in my life.",
@@ -84,8 +84,8 @@ export const events = [
 		icon: <SchoolIcon className={styles.icon} />
     },
     {
-		primary: "Our First Pet",
-		secondary: "We got a cute Netherland Dwarf Rabbit named <b>Oreo</b>.",
+		primary: "My First Pet",
+		secondary: "I got a cute Netherland Dwarf Rabbit named <b>Oreo</b>.",
 		time: new Date("January 17, 2023"),
 		image: "",
 		icon: <PetsIcon className={styles.icon} />
@@ -97,13 +97,20 @@ export const events = [
 		image: "",
 		icon: <WorkIcon className={styles.icon} />
     },
+	// {
+	// 	primary: "Our Second Pet",
+	// 	secondary: "We got a beautiful Siberian Neva Cat named <b>Simba</b>.",
+	// 	time: new Date("December 30, 2023"),
+	// 	image: "",
+	// 	icon: <PetsIcon className={styles.icon} />
+    // },
 	{
-		primary: "Our Second Pet",
-		secondary: "We got a beautiful Siberian Neva Cat named <b>Simba</b>.",
-		time: new Date("December 30, 2023"),
+		primary: "Senior Developer",
+		secondary: "I have been promoted to Senior Developer at <b>Radley Associates</b>.",
+		time: new Date("November 10, 2024"),
 		image: "",
-		icon: <PetsIcon className={styles.icon} />
-    },
+		icon: <WorkIcon className={styles.icon} />
+    }
 ];
 
 export const lengthOfGapBetweenEachItem = "35px";
